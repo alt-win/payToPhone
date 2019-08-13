28f609b41684da8794e20afb3068fd86abac2511
Alterntive_Winter <38795952+alt-win@users.noreply.github.com>
2019-08-12 22:22:37 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -37,35 +37,35 @@
 //     return; //the following code will only run if no errors above were detected
 //
 //   }
-  var modal = document.getElementById("myModal");
+//var modal = document.getElementById("myModal");
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks on the button, open the modal
-  function openModal() {
-    modal.style.display = "block";
-    console.log("button clicked");
-  }
+// When the user clicks on the button, open the modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+  console.log("button clicked");
+}
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
 
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-  //var saveNumberToContacts = document.getElementById('saveNumberToContacts').checked;
-  //if (saveNumberToContacts) {
-    //$(".enterName").css("display", "block");
-  //}
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     document.getElementById("myModal").style.display = "none";
+//   }
+// }
+//var saveNumberToContacts = document.getElementById('saveNumberToContacts').checked;
+//if (saveNumberToContacts) {
+//$(".enterName").css("display", "block");
+//}
 
 //   bchAmount = getBCHAmount();
 //   var url = "https://pay.cointext.io/p/" + phoneNumber + "/" + (bchAmount * 100000000).toString();
